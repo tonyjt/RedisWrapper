94c0f311dfa97d2ed08a5c7c9742a9e2c08f5715
tonyjt <tonyliujiangtao@gmail.com>
2013-03-19 23:05:23 +0800

    0.1
    add configuration

---
+++ RedisWrapper/RedisWrapper/RedisWrapperConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using CustomExtension;
+
+namespace RedisWrapper
+{
+    public class RedisWrapperConfiguration:ConfigurationBase
+    {
+        private string header;
+
+        public string Header { get { return header; } }
+
+        public static RedisWrapperConfiguration GetConfig()
+        {
+            return GetConfig<RedisWrapperConfiguration>("redis", "cache",true);
+        }
+
+        protected override void LoadValuesFromConfigurationXml(XmlNode node)
+        {
+            header = node.GetStringAttribute("header","redis");
+        }
+    }
+}
